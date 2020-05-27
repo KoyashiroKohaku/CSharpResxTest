a80e0fb8b2807561455fa44b34ce7b6b1fe453e5
Koyashiro Kohaku <6698252+KoyashiroKohaku@users.noreply.github.com>
2020-05-27 17:40:14 +0900

    create resx sample projects

---
+++ src/ResxTestConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using ResxHelper;
+
+namespace ResxTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resources.Culture = new CultureInfo("ja-JP");
+            Console.WriteLine($"日本語：{Resources.HellowWorld}");
+
+            Resources.Culture = new CultureInfo("en-US");
+            Console.WriteLine($"英語：{Resources.HellowWorld}");
+        }
+    }
+}
